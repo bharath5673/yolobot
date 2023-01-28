0f2e174e8bccd77c49301911c8b8c2528cab0870
bharath5673 <s.bharath.2000@gmail.com>
2023-01-28 17:36:22 +0530

    first commit

---
+++ src/yolobot_recognition/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'yolobot_recognition'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bharath',
+    maintainer_email='bharath@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'yolov5n_demo = yolobot_recognition.demo:main',
+            'yolobot_demo = yolobot_recognition.ros_recognition_yolo:main'
+
+        ],
+    },
+)
